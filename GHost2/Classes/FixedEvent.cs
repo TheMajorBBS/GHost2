d338a92819fa4f375224acd5efe85c8cf55e5049
Shawn Rapp <shawn@wmsi.co>
2021-04-04 05:03:33 -0700

    Added fixed event system for startup and shutdown.

---
+++ GHost2/Classes/FixedEvent.cs
@@ -0,0 +1,68 @@
+﻿using RandM.RMLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorBBS.GHost.Classes
+{
+    /**
+     * The FixedEvent is triggered in code and executes a powershell script at a OS level
+     */
+    class FixedEvent
+    {
+        static string powerShellCmd = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
+
+        public FixedEvent(string eventName)
+        {
+            string eventFileName = string.Format(
+                "fixed-events\\{0}.ps1",
+                eventName
+            );
+            string eventCmd = Path.Combine(ProcessUtils.StartupPath, eventFileName);
+            if (!File.Exists(eventCmd)) return;
+
+            Process proc = new Process();
+            ProcessStartInfo PSI = new ProcessStartInfo(FixedEvent.powerShellCmd, eventCmd)
+            {
+                WindowStyle = ProcessWindowStyle.Minimized,
+                WorkingDirectory = ProcessUtils.StartupPath,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+            };
+
+            proc.StartInfo = PSI;
+            proc.Start();
+            
+            while (!proc.HasExited)
+            {
+                string errorStr = proc.StandardError.ReadLine();
+                if (errorStr != "" && errorStr != null)
+                {
+                    string errFormat = String.Format(
+                        "FIXED EVENT ERROR [{0}]: {1}",
+                        eventName,
+                        errorStr
+                    );
+                    RMLog.Error(errFormat);
+                }
+
+                string conStr = proc.StandardOutput.ReadLine();
+                if (conStr != "" && conStr != null)
+                {
+                    string conFormat = string.Format(
+                        "FIXED EVENT {0}: {1}",
+                        eventName,
+                        conStr
+                    );
+                    RMLog.Info(conStr);
+                }
+            }
+        }
+    }
+}
